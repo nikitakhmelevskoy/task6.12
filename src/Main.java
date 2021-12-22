0c546abde30e2e433393954ef9408dd4cca70251
nikita <nikita03.wot@gmail.com>
2021-12-22 03:19:41 +0300

    v 0.4

--- src/Main.java
+++ src/Main.java
@@ -32,15 +32,15 @@ public class Main {
         return (num <= -1 || num >= 1);
     }
 
-    static boolean isNPositiveAndInteger(double num) {
-        return (num < 0 && ( num % 2 != 0 && num % 2 != 0));
+    static boolean isNPositiveAndInteger(int num) {
+        return !(num > 0 && num % 1 == 0);
     }
 
-    static double nextTerm(double last, int i, double x) {
-        return last * (i - 2) * x * x / i;
+    static double nextTerm(double previous, int i, double x) {
+        return previous * (i - 2) * x * x / i;
     }
 
-    static double calculateSumN(double x, int n) {
+    static double calculateSumOfSequence(double x, int n) {
         double firstTerm = x;
         double sum = 0;
 
@@ -51,7 +51,7 @@ public class Main {
         return sum * 2;
     }
 
-    static double calculateSumE(double x, double e, int n) {
+    static double calculateSumOfSequenceMoreThanEpsilon(double x, double e, int n) {
         double sum = 0;
         double firstTerm = x;
 
@@ -66,20 +66,20 @@ public class Main {
         return sum * 2;
     }
 
-    static double calculateLn(double x) {
+    static double calculateLogarithm(double x) {
         return Math.log((1 + x) / (1 - x));
     }
 
-    static void writeSumN(double sumN) {
+    static void writeSumOfSequence(double sumN) {
         System.out.println("The sum of n terms of a given form = " + sumN);
     }
 
-    static void writeSumE(double sumE, String e) {
+    static void writeSumOfSequenceMoreThanEpsilon(double sumE, String e) {
         System.out.println("The sum of those terms that are greater in absolute value than e" + e + " = " + sumE);
     }
 
-    static void writeLn(double SumLN) {
-        System.out.println("Function value using Math methods = " + SumLN);
+    static void writeLogarithm(double SumLogarithm) {
+        System.out.println("Function value using Math methods = " + SumLogarithm);
     }
 
     static void printResults(double num1, int num2, double num3) {
@@ -88,10 +88,10 @@ public class Main {
         int n = num2;
         double e = num3;
 
-        writeSumN(calculateSumN(x, n));
-        writeSumE(calculateSumE(x, e, n), "");
-        writeSumE(calculateSumE(x, e / 10, n), "/10");
-        writeLn(calculateLn(x));
+        writeSumOfSequence(calculateSumOfSequence(x, n));
+        writeSumOfSequenceMoreThanEpsilon(calculateSumOfSequenceMoreThanEpsilon(x, e, n), "");
+        writeSumOfSequenceMoreThanEpsilon(calculateSumOfSequenceMoreThanEpsilon(x, e / 10, n), "/10");
+        writeLogarithm(calculateLogarithm(x));
     }
 
 }
